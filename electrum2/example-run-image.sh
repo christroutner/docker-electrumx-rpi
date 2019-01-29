8f10ee6bfd895c3e60f4550092aae7b20019bed4
Chris Troutner <chris.troutner@gmail.com>
2019-01-29 15:00:16 -0800

    updating example run script

--- electrum2/example-run-image.sh
+++ electrum2/example-run-image.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 docker container run --name electrumx2 -d \
 -p 50001:50001 \
+# USB flash drive path for storing the blockchain database.
+#-v /media/usb/mydrive:/srv/db \
+# Connection information to the BCH full node.
+#-e DAEMON_URL=http://rpcuser:rpcpass@ipaddr:port \
 electrumx2
